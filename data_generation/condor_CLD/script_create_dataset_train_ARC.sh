866116abd4dcd0f48e82906b1af6cc92af4e1605
Vincent Riechers <v.riechers@outlook.com>
2026-03-04 15:46:02 +0100

    Update ARC dataset creation condor script

--- data_generation/condor_CLD/script_create_dataset_train_ARC.sh
+++ data_generation/condor_CLD/script_create_dataset_train_ARC.sh
@@ -1,28 +1,14 @@
 #!/bin/bash
 
-# gun
-# python submit_jobs_train_ARC.py \
-#   --sample gun \
-#   --cldgeo CLD_o2_v05 \
-#   --config config_spread_eCH.gun \
-#   --outdir /eos/experiment/fcc/users/m/mgarciam/mlpf/CLD/train/gun_perfect_tracking_CHe_3/ \
-#   --condordir /eos/experiment/fcc/users/m/mgarciam/mlpf/condor/gun_perfect_tracking_CHe_3/ \
-#   --njobs 2 \
-#   --nev 20 \
-#   --queue tomorrow \
-#   --cldconfig /afs/cern.ch/work/m/mgarciam/private/CLD_Config_versions/CLDConfig_ARC/CLDConfig/
-
-
-# pythia
-python submit_jobs_train_ARC.py \
-  --sample Zcard \
+python3 submit_jobs_train_ARC.py \
+  --sample gun \
   --cldgeo CLD_o2_v05 \
-  --config p8_ee_Zuds_ecm91 \
-  --outdir /eos/experiment/fcc/users/m/mgarciam/mlpf/CLD/train/Z_uds_clustering_dataset_2/ \
-  --condordir /eos/experiment/fcc/users/m/mgarciam/mlpf/condor/Z_uds_clustering_dataset_2/ \
-  --njobs 2 \
-  --nev 1 \
+  --config config_spread_031224_fair.gun \
+  --outdir /eos/experiment/fcc/ee/simulation/key4hep_2024_10_03/91GeV/CLD_ARC/500k_training \
+  --condordir /eos/experiment/fcc/ee/simulation/key4hep_2024_10_03/91GeV/CLD_ARC/500k_training/condor \
+  --njobs 5000 \
+  --nev  100 \
   --queue tomorrow \
-  --cldconfig /afs/cern.ch/work/m/mgarciam/private/CLD_Config_versions/CLDConfig_ARC/CLDConfig/ # \
-#   --arc \
-#   --gentracking
+  --cldconfig /eos/user/v/vriecher/mlpf_arc/CLDConfig_ARC/CLDConfig \
+  --arc \
+  --gentracking
